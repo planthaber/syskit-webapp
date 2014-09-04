978ba20b8c211671ff2fbe1e043e15fd62c9cd71
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2014-09-04 11:55:58 +0200

    Initial commit

---
+++ lib/syskit-webapp.rb
@@ -0,0 +1,11 @@
+require 'syskit-webapp/syskit-webapp'
+require 'syskit-webapp/base'
+
+# The toplevel namespace for syskit-webapp
+#
+# You should describe the basic idea about syskit-webapp here
+require 'utilrb/logger'
+module Syskit-webapp
+    extend Logger::Root('Syskit-webapp', Logger::WARN)
+end
+
